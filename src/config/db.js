6507cd0dcd482274de784597b23a8c2a0b6b9ada
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-01-17 01:10:35 +0300

    changed

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const mongooseDB = async()=>{
+        try{
+         await mongoose.connect(process.env.MONGO_URI);
+         console.log("Mogooes is Connected");       
+        }catch(err){
+                console.error("DB Error: " ,err.message);
+                process.exit(1);
+        }
+};
+module.exports = mongooseDB;
