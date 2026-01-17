8310203251ba1fff76453e7365f26f4983717948
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-01-17 18:04:35 +0300

    Day one part one

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
-const mongooseDB = async()=>{
-        try{
-         await mongoose.connect(process.env.MONGO_URI);
-         console.log("Mogooes is Connected");       
-        }catch(err){
-                console.error("DB Error: " ,err.message);
-                process.exit(1);
-        }
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in .env");
+    }
+
+    // Connect to MongoDB
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB is connected ✅");
+  } catch (err) {
+    console.error("DB Error:", err.message);
+    process.exit(1);
+  }
 };
-module.exports = mongooseDB;
+
+module.exports = connectDB;
